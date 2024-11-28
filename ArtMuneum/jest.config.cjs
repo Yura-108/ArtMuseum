859e96c6db6844500036529043e5b3825c80d5ab
Yury Poddubny <70597955+Yura-108@users.noreply.github.com>
2024-11-28 16:57:36 +0300

    configured project

---
+++ ArtMuneum/jest.config.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "api/", "e2e/"],
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    "^@pages(.*)$": "<rootDir>/src/pages$1",
+    "^@assets(.*)$": "<rootDir>/src/assets$1",
+    "^@constants(.*)$": "<rootDir>/src/constants$1",
+  },
+  globals: {
+    __PATH_PREFIX__: "",
+  },
+  verbose: true,
+  transform: {
+    "^.+\\.[jt]sx?$": "<rootDir>/jest.transform.cjs",
+  },
+  roots: ["."],
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  testEnvironment: "jsdom",
+
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.styled.ts",
+    "!src/**/*.d.ts",
+    "!src/index.tsx",
+    "!src/serviceWorker.ts",
+    "!src/setupTests.ts",
+  ],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+};
---
+++ ArtMuneum/jest.transform.cjs
@@ -0,0 +1,18 @@
+const babelOptions = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: 18,
+        },
+      },
+    ],
+    ["@babel/react", {
+      "runtime": "automatic"
+    }],
+    "@babel/preset-typescript",
+  ],
+};
+
+module.exports = require("babel-jest").default.createTransformer(babelOptions);
