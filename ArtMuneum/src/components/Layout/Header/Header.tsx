6148b7f5ffb725ff1e1860e5bfc56827f0f16242
Yury Poddubny <70597955+Yura-108@users.noreply.github.com>
2024-11-28 17:07:00 +0300

    Squashed commit of the following:
    commit 7be8a46e1fcaf043aac5d0599b3cff89a8b91f42
    Author: Yury Poddubny <70597955+Yura-108@users.noreply.github.com>
    Date:   Thu Nov 28 17:06:48 2024 +0300
    
        Add Footer and Header components

---
+++ ArtMuneum/src/components/Layout/Header/Header.tsx
@@ -0,0 +1,33 @@
+import './Header.scss';
+import logo from '@images/museum-logo.svg';
+import favoriteIcon from '@images/favoriteIcon.svg';
+import burgerIcon from '@images/burgerMenuIcon.svg';
+import { Link } from 'react-router-dom';
+import useBurgerMenu from '../../../utils/hooks/useBurgerMenu.ts';
+
+export default function Header() {
+  const {isOpen, openMenu, closeMenu} = useBurgerMenu();
+
+  return (
+    <header>
+      <div className="container">
+        <Link to={'/'}>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+        </Link>
+        <img
+          onClick={isOpen ? closeMenu : openMenu}
+          className={"burgerButton"} src={burgerIcon}
+          alt="burgerIcon"
+        />
+        <Link
+          to={'/favorites'}
+          className={`burgerMenu ${isOpen ? 'open' : ''}`}>
+          <img src={favoriteIcon} alt="favorite" />
+          <span>Your favorites</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
